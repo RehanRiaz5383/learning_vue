b7db4f4068560ad9412608d00404786376f5185e
Rehan Riaz <rehan5383@gmail.com>
2023-03-16 18:32:09 +0500

    code complition done

--- modal-project/src/App.vue
+++ modal-project/src/App.vue
@@ -1,27 +1,46 @@
 <template>
-  <h1>{{title}}</h1>
+  <div class="game">
+    <h1>{{title}}</h1>
+    <button @click="start" :disabled="isPlaying">play</button>
+    <Block v-if="isPlaying" @end="end" :delay="delay"/>
+    <p v-if="showResults">Your score is {{score}} ms</p>
+  </div>
 </template>
-
+<style scoped>
+  .game{
+    width:800px;
+    margin:0 auto;
+    align-items: center;
+    text-align:center;
+  }
+</style>
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/Modal.vue';
+import Block from './components/Block';
 
 export default {
+  components:{Modal,Block},
   name: 'App',
   data(){
     return {
-      title:"Hello world"
+      title:"Hello world",
+      isPlaying:false,
+      delay:null,
+      score:null,
+      showResults:false
     }
+  },
+  methods:{
+   start(){
+    this.delay = 2000 + Math.random()*5000;
+    this.isPlaying = true;
+    this.showResults = false;
+   },
+   end(score){
+    this.score = score;
+    this.showResults = true;
+    this.isPlaying = false;
+   }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
